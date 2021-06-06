f2a1051ed43bd6ff3a4cb5117a3e6d0ab21cc85a
k1kli <karolklimaszewski0@gmail.com>
2021-06-06 16:39:48 +0200

    Update plots dependencies windows script

--- windows/plots_dependencies.bat
+++ windows/plots_dependencies.bat
@@ -1,1 +1,2 @@
 echo if (!require("ggplot2")) install.packages("ggplot2", repos="https://cran.rstudio.com") | R --no-save
+echo if (!require("emojifont")) install.packages("emojifont", repos="https://cran.rstudio.com") | R --no-save

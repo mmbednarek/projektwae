8d0af92782dbdff95c708d053e91762df21f4708
k1kli <karolklimaszewski0@gmail.com>
2021-06-10 18:03:18 +0200

    Update test.py to run tests multiple times

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,7 @@ from math import sin, cos, sqrt
 from projektwae.evolution import differential_evolution, differential_evolution_dg
 from projektwae.util import get_error, as_handler, IterationLog
 
-seed = 10000000
+base_seed = 10000000
 
 
 def get_seed():
@@ -22,10 +22,10 @@ def get_seed():
     return random.randint(0, 1000000000)
 
 
-def get_log_path(testname, diversity_guided):
+def get_log_path(testname, diversity_guided, attempt):
     if diversity_guided:
-        return "logs/" + testname + ".dg.csv"
-    return "logs/" + testname + ".csv"
+        return "logs/" + testname + ".dg." + str(attempt) + ".csv"
+    return "logs/" + testname + "." + str(attempt) + ".csv"
 
 
 class TestCaseBuilder:
@@ -36,7 +36,8 @@ class TestCaseBuilder:
         self.expected_minimum = None
         self.expected_minimum_set = None
         self.expected_minimum_toleration = None
-        self.seed = seed
+        self.base_seed = base_seed
+        self.attempts = 1
 
     def with_bounds(self, bounds):
         self.bounds = bounds
@@ -48,6 +49,10 @@ class TestCaseBuilder:
 
     def with_iteration_count(self, count):
         self.iteration_count = count
+        return self
+
+    def with_attempts(self, attempts):
+        self.attempts = attempts
         return self
 
     def expect_minimum(self, minimum, toleration):
@@ -78,6 +83,9 @@ class TestCaseBuilder:
         return float("inf")
 
     def run(self, diversity_guided=False):
+        r = random.Random()
+        r.seed(self.base_seed)
+        seeds = [r.randint(0, 1000000000) for _ in range(0, self.attempts)]
         if self.bounds == None or self.target_func == None:
             self.test_handle.fail("test lacks bounds or target func")
             return self
@@ -86,19 +94,20 @@ class TestCaseBuilder:
         if diversity_guided:
             evolution_func = differential_evolution_dg
 
-        with IterationLog(get_log_path(self.name, diversity_guided), len(self.bounds)) as logger:
-            point, value = None, None
-            for result in evolution_func(as_handler(self.target_func), self.bounds, self.seed,
-                                         iteration_count=self.iteration_count):
-                point, value, diversity = result
-                logger.log(point, value, self.get_error(point), abs(self.expected_minimum_value - value), diversity)
+        for attempt_id, attempt_seed in enumerate(seeds):
+            with IterationLog(get_log_path(self.name, diversity_guided, attempt_id), len(self.bounds)) as logger:
+                point, value = None, None
+                for result in evolution_func(as_handler(self.target_func), self.bounds, attempt_seed,
+                                             iteration_count=self.iteration_count):
+                    point, value, diversity = result
+                    logger.log(point, value, self.get_error(point), abs(self.expected_minimum_value - value), diversity)
 
-        if not diversity_guided and self.expected_minimum_toleration != None:
-            self.test_handle.assertLessEqual(self.get_error(point), self.expected_minimum_toleration)
+            if not diversity_guided and self.expected_minimum_toleration != None:
+                self.test_handle.assertLessEqual(self.get_error(point), self.expected_minimum_toleration)
 
-        if not diversity_guided and self.expected_minimum_value != None:
-            self.test_handle.assertLessEqual(abs(self.expected_minimum_value - value),
-                                             self.expected_minimum_value_toleration)
+            if not diversity_guided and self.expected_minimum_value != None:
+                self.test_handle.assertLessEqual(abs(self.expected_minimum_value - value),
+                                                 self.expected_minimum_value_toleration)
 
         return self
 
@@ -119,6 +128,7 @@ class OneOptimumTest(unittest.TestCase):
         TestCaseBuilder(self, "one.parabola") \
             .with_target_func(lambda x, y: x ** 2 + y ** 2) \
             .with_bounds([(-10, 10), (-10, 10)]) \
+            .with_attempts(10) \
             .expect_minimum([0, 0], toleration=1e-07) \
             .expect_minimum_value(0, toleration=1e-15) \
             .run(diversity_guided=False) \
@@ -128,6 +138,7 @@ class OneOptimumTest(unittest.TestCase):
         TestCaseBuilder(self, "one.transposed_parabola") \
             .with_target_func(lambda x, y, z: (x - 2) ** 2 + (y + 1) ** 2 + (z - 6) ** 2) \
             .with_bounds([(-20, 20), (-20, 20), (-20, 20)]) \
+            .with_attempts(10) \
             .expect_minimum([2, -1, 6], toleration=1e-4) \
             .expect_minimum_value(0, toleration=1e-8) \
             .run(diversity_guided=False) \
@@ -139,6 +150,7 @@ class TwoLocalOneGlobalTest(unittest.TestCase):
         TestCaseBuilder(self, "two_local.single_dimension") \
             .with_target_func(lambda x: x ** 4 - 4.3 * x ** 3 + 3 * x ** 2 - 3) \
             .with_bounds([(-1, 3)]) \
+            .with_attempts(10) \
             .with_iteration_count(50) \
             .expect_minimum([8 / 3], toleration=1e-2) \
             .expect_minimum_value(-12.639506, toleration=1e-3) \
@@ -150,6 +162,7 @@ class TwoLocalOneGlobalTest(unittest.TestCase):
             .with_target_func(lambda x, y: -x ** 5 + 2 * x ** 4 + 4 * x ** 3 - 2 * x ** 2 - 2 * x * y - 2 * x + y ** 2) \
             .with_bounds([(-2, 2), (-2, 2)]) \
             .with_iteration_count(100) \
+            .with_attempts(10) \
             .expect_minimum([-1.14498, -1.14498], toleration=1e-4) \
             .expect_minimum_value(-2.24199, toleration=1e-5) \
             .run(diversity_guided=False) \
@@ -162,6 +175,7 @@ class TwoGlobalTest(unittest.TestCase):
             .with_target_func(lambda x: x ** 4 - 2 * x ** 2) \
             .with_bounds([(-2, 2)]) \
             .with_iteration_count(50) \
+            .with_attempts(10) \
             .expect_either_minumum([[-1], [1]], toleration=1e-5) \
             .expect_minimum_value(-1, toleration=1e-11) \
             .run(diversity_guided=False) \
@@ -173,6 +187,7 @@ class MultipleLocalTest(unittest.TestCase):
         TestCaseBuilder(self, "multiple_local") \
             .with_target_func(lambda x, y: -5 * cos(x ** 2 + y ** 2) / (sqrt(x ** 2 + y ** 2 + 1))) \
             .with_bounds([(-8, 8), (-8, 8)]) \
+            .with_attempts(10) \
             .expect_minimum([0, 0], toleration=1e-5) \
             .expect_minimum_value(-5, toleration=1e-10) \
             .run(diversity_guided=False) \
@@ -185,12 +200,14 @@ class MultipleGlobal(unittest.TestCase):
             .with_target_func(lambda x, y: -2 * sin(x ** 2 + y ** 2) / sqrt(x ** 2 + y ** 2)) \
             .with_bounds([(-8, 8), (-8, 8)]) \
             .with_iteration_count(20) \
+            .with_attempts(10) \
             .expect_minimum_value(-1.7025, toleration=1e-1) \
             .run(diversity_guided=False) \
             .run(diversity_guided=True)
 
+if __name__ == '__main__':
+    base_seed = get_seed()
+    print("seed: " + str(base_seed))
 
 if __name__ == '__main__':
-    seed = get_seed()
-    print("seed: " + str(seed))
     unittest.main()
